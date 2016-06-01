mauricewells/react-boilerplate

commit 80da0c517e94b9c78614773b94be10427f4083fc

re install pre-commit after setup
fixes #371

internals/scripts/setup.js
@@ -38,8 +38,16 @@ cleanRepo(dir, function () {
       process.stdout.write('Initialising new repository');
       initGit(dir, function () {
         clearInterval(interval);
-        process.stdout.write('\nDone!');
-        process.exit(0);
+
+        process.stdout.write('\n');
+        interval = animateProgress('Initialising pre-commit');
+        process.stdout.write('Initialising pre-commit');
+        installPreCommit(function () {
+          clearInterval(interval);
+
+          process.stdout.write('\nDone!');
+          process.exit(0);
+        });
       });
     });
   });
@@ -57,6 +65,13 @@ function cleanRepo(dir, callback) {
  */
 function initGit(dir, callback) {
   exec('git init && git add . && git commit -m "Initial commit"', addCheckMark.bind(null, callback));
+}
+
+/**
+ * Install pre-commit, needed because we have just deleted the old git repository
+ */
+function installPreCommit(callback) {
+  exec('npm install pre-commit', addCheckMark.bind(null, callback));
 }
 
 /**

commit 02bbe5482ecc23b38ee6e1dd851e745e3f52ba83

Undo commit 80da0c5 which caused npm run clean to fail

internals/scripts/setup.js
@@ -38,16 +38,8 @@ cleanRepo(dir, function () {
       process.stdout.write('Initialising new repository');
       initGit(dir, function () {
         clearInterval(interval);
-
-        process.stdout.write('\n');
-        interval = animateProgress('Initialising pre-commit');
-        process.stdout.write('Initialising pre-commit');
-        installPreCommit(function () {
-          clearInterval(interval);
-
-          process.stdout.write('\nDone!');
-          process.exit(0);
-        });
+        process.stdout.write('\nDone!');
+        process.exit(0);
       });
     });
   });
@@ -65,13 +57,6 @@ function cleanRepo(dir, callback) {
  */
 function initGit(dir, callback) {
   exec('git init && git add . && git commit -m "Initial commit"', addCheckMark.bind(null, callback));
-}
-
-/**
- * Install pre-commit, needed because we have just deleted the old git repository
- */
-function installPreCommit(callback) {
-  exec('npm install pre-commit', addCheckMark.bind(null, callback));
 }
 
 /**